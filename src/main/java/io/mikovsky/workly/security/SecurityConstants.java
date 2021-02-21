030db3a93e623976530571509fcfaf483b03cc38
mikovsky <dudek.michal1996@gmail.com>
2021-02-21 14:25:10 +0100

    comment removed
    Signed-off-by: mikovsky <dudek.michal1996@gmail.com>

--- src/main/java/io/mikovsky/workly/security/SecurityConstants.java
+++ src/main/java/io/mikovsky/workly/security/SecurityConstants.java
@@ -5,6 +5,6 @@ public interface SecurityConstants {
     String SECRET = "SecretKeyToGenerateJWTs";
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
-    long EXPIRATION_TIME = 900_000; // 5 minutes
+    long EXPIRATION_TIME = 900_000;
 
 }
