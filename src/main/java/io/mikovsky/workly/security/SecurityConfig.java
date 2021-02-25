6364929d018466dc49f07a8da517d89e5067fa22
mikovsky <dudek.michal1996@gmail.com>
2021-02-25 22:04:00 +0100

    cors
    Signed-off-by: mikovsky <dudek.michal1996@gmail.com>

--- src/main/java/io/mikovsky/workly/security/SecurityConfig.java
+++ src/main/java/io/mikovsky/workly/security/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http
+                .csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
